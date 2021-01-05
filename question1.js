5b7afcc8c2f2fff250179b07371a8adab76941f8
FatmaHamad-M1-56 <76535095+FatmaHamad-M1-56@users.noreply.github.com>
2021-01-05 22:04:47 +0300

    fatma

--- question1.js
+++ question1.js
@@ -1,1 +1,14 @@
 // قم بحل السؤال الأول هنا
+let bets =['rabbit','donky','sheep','cat','dog']
+console.log(bets);
+bets.pop();
+console.log(bets);
+bets.push('monkey');
+console.log(bets);
+let rabbit ={
+name:'rabbit',
+Age:'3',
+Type:'soft'
+}
+console.log(rabbit);
+console.log('the rabbit age ${rabbit.Age}');
